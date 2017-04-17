9c9e63aedd05a9822b08e6d3fabd1aa82e7c5619
Beth Evans <baevans_87@hotmail.com>
2017-04-16 22:14:32 -0500

    toggle cake menu added

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+
+$("fixed-button").click(function(){
+  $("cake-types other").hide();
+  $("cake-types cakes").show();
+});
+$("fixed-reverse").click(function(){
+  $("cake-types cakes").hide();
+  $("cake-types other").show();
+});
