942b65d5874a733ef0a7f6067870662ee7e2de2d
Beth Evans <baevans_87@hotmail.com>
2017-04-17 20:03:44 -0500

    removed linking for cakes v other, js working to toggle menu pages

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,14 @@
-
-$("fixed-button").click(function(){
-  $("cake-types other").hide();
-  $("cake-types cakes").show();
-});
-$("fixed-reverse").click(function(){
-  $("cake-types cakes").hide();
-  $("cake-types other").show();
+$(document).ready(function(){
+  $(".button-cakes").click(function() {
+    $(".cake-types.other").hide();
+    $(".cake-types.cakes").show();
+    $(".button-cakes").children().removeClass("disabled").addClass("highlighted");
+    $(".button-other").children().removeClass("highlighted").addClass("disabled");
+  });
+  $(".button-other").click(function() {
+    $(".cake-types.cakes").hide();
+    $(".cake-types.other").show();
+    $(".button-other").children().removeClass("disabled").addClass("highlighted");
+    $(".button-cakes").children().removeClass("highlighted").addClass("disabled");
+  });
 });
