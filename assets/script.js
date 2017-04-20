2bb83290fc6753490b84556323eccee2ac1f4c46
Beth Evans <baevans_87@hotmail.com>
2017-04-19 19:55:39 -0500

    submit form functionality completed

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,28 @@
 $(document).ready(function(){
-  $(".button-cakes").click(function() {
-    $(".cake-types.other").hide();
-    $(".cake-types.cakes").show();
+  $(".button-cakes").on('click', 'h1', function() {
     $(".button-cakes").children().removeClass("disabled").addClass("highlighted");
     $(".button-other").children().removeClass("highlighted").addClass("disabled");
+    $(".cake-types.other").hide();
+    $(".cake-types.cakes").show();
   });
   $(".button-other").click(function() {
-    $(".cake-types.cakes").hide();
-    $(".cake-types.other").show();
     $(".button-other").children().removeClass("disabled").addClass("highlighted");
     $(".button-cakes").children().removeClass("highlighted").addClass("disabled");
+    $(".cake-types.cakes").hide();
+    $(".cake-types.other").show();
   });
+
+
+
+  $("form").submit(function() {
+    var name = document.getElementsByName("firstname")[0].value;
+    $("#after-form").show();
+    $("form").hide();
+
+    alert("Thank you for your order, " + name + ". " +
+    "We will get back to you shortly!");
+   });
+   $("form").submit(function(event){
+      event.preventDefault();
+    });
 });
